51e184cbffa84550bcc8f35ea5eb730bcd2e5042
Andrew Mei <andrewmei915@gmail.com>
2020-04-13 13:22:51 -0400

    first commit

--- Nano_EEPROM_Programmer.ino
+++ Nano_EEPROM_Programmer.ino
@@ -1,13 +1,21 @@
 #include <digitalWriteFast.h>
-//must use const int or #define to use digitalWriteFast
+//must use const int or #define to use digitalWriteFast() function on pins
 #define RCLK 4
 #define SER 2
 #define SRCLK 3
+
 #define OUT_EN_ACTIVE_LOW A0
 #define WR_EN_ACTIVE_LOW 13
 
 #define EEPROM_D0 5
 #define EEPROM_D7 12
+
+/* 
+ *        INSERT DATA INTO ARRAY WITH ADDRESSES LOCATED RESPECTIVELY
+ * Example: data for 4-bit hex decoder for common cathode 7-segment display (5161AS)
+  */
+byte data[] = { 0x7e, 0x30, 0x6d, 0x79, 0x33, 0x5b, 0x5f, 0x70, 0x7f, 0x7b, 0x77, 0x1f, 0x4e, 0x3d, 0x4f, 0x47 };
+
 
 
 
@@ -25,13 +33,13 @@ void setAddress(int address, bool OUT_EN) {
 
 //returns the byte of data in the EEPROM
 byte readEEPROM(int address) {
-  for (int pin = EEPROM_D0; pin <= EEPROM_D7; pin++) {pinMode(pin, INPUT);} //set pins as input
+  for (int pin = EEPROM_D0; pin <= EEPROM_D7; pin++) {pinModeFast(pin, INPUT);} //set pins as input
   setAddress(address, true);
   byte data = B00000000;
 
   //read data
   for (int pin = EEPROM_D7; pin >= EEPROM_D0; pin--) {
-    data = (data << 1) + digitalRead(pin);
+    data = (data << 1) + digitalReadFast(pin);
   }
   return data;
 }
@@ -41,45 +49,53 @@ byte readEEPROM(int address) {
 //Write a byte to the EEPROM at the specified address.
 void writeEEPROM(int address, byte data) {
   setAddress(address, false);
-  for (int pin = EEPROM_D0; pin <= EEPROM_D7; pin++) {pinMode(pin, OUTPUT);}
+  for (int pin = EEPROM_D0; pin <= EEPROM_D7; pin++) {pinModeFast(pin, OUTPUT);}
 
   for (int pin = EEPROM_D0; pin <= EEPROM_D7; pin++) {
-    digitalWriteFast(pin, data & 1); //ANDing with 1 has the result of outputting the last digit
+    digitalWriteFast(pin, data & 1); //ANDing data with 1 only ANDs the last digit
     data = data >> 1; //shifts to the next bit
   }
-  
+  //120ns WR_EN pulse (has to be between 100-1000ns)
   digitalWriteFast(WR_EN_ACTIVE_LOW, LOW);
   digitalWriteFast(WR_EN_ACTIVE_LOW, HIGH);
-  delay(10);
+  
+  //DATA POLLING to await end of write cycle
+  bool EEPROM_D7_VALUE = readEEPROM(address) & 1;
+  while (EEPROM_D7_VALUE != data & 1) { //wait until D7 equals data being written
+      EEPROM_D7_VALUE = readEEPROM(address) & 1; 
+  } 
 }
 
 
 
-//prints data in first 256 addresses onto serial monitor
+//Prints data in first 256 addresses onto serial monitor
 void printContents() {
-  //creates a list (data) which contains 16 bytes of EEPROM's data
-  for (int BYTES = 0; BYTES < 256/*loops 16 times*/; BYTES += 16) {
-    byte data[16];
+  //Creates a list (called "data") which contains 16 bytes of EEPROM's data
+  /*
+   * Change 256 (in for loop) to print data in that number of addresses. Number must be divisible by the number of bytes per line (default:8)
+   */
+  for (int rowOfBytes = 0; rowOfBytes < 256; rowOfBytes += 8) {
+    byte data[8]; //8 addresses each line 
 
-    for (int BIT = 0; BIT <= 15; BIT++) {
-      data[BIT] = readEEPROM(BYTES + BIT);
+    for (int BIT = 0; BIT <= 7; BIT++) {
+      data[BIT] = readEEPROM(rowOfBytes + BIT);
     }
 
-    //prints a line of 16 bytes
+    //Prints formatted line of bytes
     char buf[80];
-    sprintf(buf, "%03B:  %02B %02B %02B %02B %02B %02B %02B %02B  %02B %02B %02B %02B %02B %02B %02B %02B",
-            BYTES, data[0], data[1], data[2], data[3], data[4], data[5], data[6], data[7],
-            data[8], data[9], data[10], data[11], data[12], data[13], data[14], data[15]);
+    sprintf(buf, "%03B:  %02B %02B %02B %02B  %02B %02B %02B %02B",
+            rowOfBytes, data[0], data[1], data[2], data[3], data[4], data[5], data[6], data[7]);
     Serial.println(buf);
   }
 }
 
 
 
+//erase entire 28C16
 void clearEEPROM() {
   Serial.print("Erasing EEPROM");
   for (int address = 0; address <= 2047; address += 1) {
-    writeEEPROM(address, 0xff);
+    writeEEPROM(address, B11111111);
     if (address % 64 == 0) {Serial.print(".");} //loading effect
   }
   Serial.println(" done");
@@ -87,34 +103,27 @@ void clearEEPROM() {
 
 
 
-/*
- * data for 4-bit hex decoder for common cathode 7-segment display (5161AS)
- */
-byte data[] = { 0x7e, 0x30, 0x6d, 0x79, 0x33, 0x5b, 0x5f, 0x70, 0x7f, 0x7b, 0x77, 0x1f, 0x4e, 0x3d, 0x4f, 0x47 };
-
-
-
 void setup() {
-  pinMode(RCLK, OUTPUT);
-  pinMode(SRCLK, OUTPUT);
-  pinMode(SER, OUTPUT);
+  digitalWriteFast(OUT_EN_ACTIVE_LOW, HIGH);
+  pinModeFast(RCLK, OUTPUT);
+  pinModeFast(SRCLK, OUTPUT);
+  pinModeFast(SER, OUTPUT);
   
   digitalWriteFast(WR_EN_ACTIVE_LOW, HIGH);
-  pinMode(WR_EN_ACTIVE_LOW, OUTPUT);
+  pinModeFast(WR_EN_ACTIVE_LOW, OUTPUT);
   Serial.begin(57600);
 
   clearEEPROM();
 
-  // Program data bytes
+  // Write into data bytes
   Serial.print("Programming EEPROM");
-  for (int address = 0; address < sizeof(data); address += 1) {
+  for (int address = 0; address < sizeof(data); address++) {
     writeEEPROM(address, data[address]);
     digitalWriteFast(WR_EN_ACTIVE_LOW, HIGH);
-    if (address % 64 == 0) {Serial.print(".");} //loading effect
+    if (address % 30 == 0) {Serial.print(".");} //loading effect
   }
   Serial.println("done");
   
-  delayMicroseconds(1); //wait to complete write cycle 
   // Read and print out the contents of the EEPROM
   Serial.println("Reading EEPROM...");
   printContents();
